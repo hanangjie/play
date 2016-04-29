75065bd24f3ca18d69a130208433bd9136c5f10c
hanangjie <hanangjie@gmail.com>
2016-04-29 11:30:28 +0800

    debug

--- pyin.js
+++ pyin.js
@@ -35,3 +35,38 @@ function ucfirst(l1){
         return first + spare;
     }
 }
+//首字母
+function codeFirst(l1){
+    var l2 = l1.length;
+    var I1 = "";
+    var reg = new RegExp('[a-zA-Z0-9\- ]');
+    for (var i=0;i<l2;i++) {
+        var val = l1.substr(i,1);
+        var name = arraySearchFirst(val,PinYin);
+        if(reg.test(val)) {
+            I1 += val;
+        } else if (name!==false) {
+            I1 += name;
+        }
+
+    }
+    I1 = I1.replace(/ /g,'-');
+    while (I1.indexOf('--')>0) {
+        I1 = I1.replace('--','-');
+    }
+    return I1;
+}
+function arraySearchFirst(l1,l2){
+    for (var name in PinYin){
+        if (PinYin[name].indexOf(l1)!=-1) {
+            return onlyfirst(name); break;
+        }
+    }
+    return false;
+}
+function onlyfirst(l1){
+    if (l1.length>0) {
+        var first = l1.substr(0,1);
+        return first;
+    }
+}
